0f62865eca7f4858625b503999f0b61c574ef5c3
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-01-27 10:51:40 -0600

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,61 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+//import Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+//reducer for feelingToday component
+const feelingToday = (state = 0, action) => {
+   if(action.type === 'SET_FEELING'){
+        console.log('In FEELING', state);
+        const addFeeling = action.payload;
+        return addFeeling; 
+    }
+        return state;
+}
+
+//Reducer for the understanding component
+const understandToday = (state = 0, action) => {
+    if(action.type === 'UNDERSTAND_TODAY'){
+        console.log('In UNDERSTAND', state );
+        const addUnderstand = action.payload;
+        return addUnderstand;
+    } 
+    return state;
+}
+
+//Reducer for the Support level
+const supportLevel = (state = 0, action) => {
+    if(action.type === 'SUPPORT_LEVEL'){
+       console.log('In SUPPORT', state);
+        const addSupport = action.payload;
+        return addSupport;
+    }
+    return state;
+}
+
+//Reducer for the additional comments 
+const addComments = (state = ' ', action) => {
+    if(action.type === 'COMMENTS'){
+        console.log('In Comments', state);
+        const comment = action.payload;
+        return comment;
+    }
+}
+
+//Create store
+const storeInstance = createStore(
+    combineReducers({
+        feelingToday,
+        understandToday,
+        supportLevel,
+        addComments,
+        
+    }),
+    applyMiddleware(logger),
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
