62b78eece64692d8ff3c45795f4a4dc7832b4049
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-06-03 12:16:01 -0500

    update Known high severity security vulnerability detected in axios <= 0.18.0 defined in package.json.

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -74,7 +74,7 @@ class Feeling extends Component {
                     </CardActions>
                 </Card>
                 <YourFeedback />
-                </div>
+            </div>
         );
     }
 }
